a7c86324bee9d1d1fe5d5968cd8889f936f64735
cmsy86 <cmsy86@naver.com>
2021-11-02 10:00:15 +0900

    2021_Portfolio_Heyy

--- script/script.js
+++ script/script.js
@@ -1,26 +1,28 @@
 $(function(){
 
     // GNB
-    $("nav#gnb li.mainLi").mouseover(function(){
+    $("nav#gnb li.mainLi:nth-child(2), nav#gnb li.mainLi:nth-child(4)").mouseover(function(){
         $(this).children("ul.subMenu").stop().slideDown(300);
+        $("div#gnbBG").stop().slideDown(300);
     });
-    $("nav#gnb li.mainLi").mouseout(function(){
+    $("nav#gnb li.mainLi:nth-child(2), nav#gnb li.mainLi:nth-child(4)").mouseout(function(){
         $(this).children("ul.subMenu").stop().slideUp(300);
+        $("div#gnbBG").stop().slideUp(300);
     });
 
-    // Slide
-    setInterval (fnSlide, 3000);
+    // MAIN Slide Show
+    setInterval (fnSlide, 6000);
     function fnSlide(){
         $("#slide img").eq(0).fadeOut(
-            3000,
+            2000,
             function(){
                 $(this).insertAfter("#slide img:last-child");
             }
         );
-        $("#slide img").eq(1).fadeIn(5000);
+        $("#slide img").eq(1).fadeIn(6000);
     }
 
-    // Search Popup
+    // MAIN Search Popup
     $("div#searchIcon i").click(function(){
         $("div#searchBG").css({"display": "block"});
     });
@@ -28,4 +30,68 @@ $(function(){
         $("div#searchBG").css({"display": "none"});
     });
 
-});
+    // HOTELS Indicators
+    new Swiper('.swiper1', {
+        loop : true,
+        pagination : {
+            el : '.swiper-pagination',
+            clickable : true,
+        },
+        navigation : {
+                nextEl : '.swiper-button-next',
+                prevEl : '.swiper-button-prev',
+        },
+    });
+
+    new Swiper('.swiper2', {
+        loop : true,
+        pagination : {
+            el : '.swiper-pagination',
+            clickable : true,
+        },
+        navigation : {
+                nextEl : '.swiper-button-next',
+                prevEl : '.swiper-button-prev',
+        },
+    });
+
+    new Swiper('.swiper3', {
+        loop : true,
+        pagination : {
+            el : '.swiper-pagination',
+            clickable : true,
+        },
+        navigation : {
+                nextEl : '.swiper-button-next',
+                prevEl : '.swiper-button-prev',
+        },
+    });
+
+    new Swiper('.swiper4', {
+        loop : true,
+        pagination : {
+            el : '.swiper-pagination',
+            clickable : true,
+        },
+        navigation : {
+                nextEl : '.swiper-button-next',
+                prevEl : '.swiper-button-prev',
+        },
+    });
+
+    new Swiper('.swiper5', {
+        loop : true,
+        pagination : {
+            el : '.swiper-pagination',
+            clickable : true,
+        },
+        navigation : {
+                nextEl : '.swiper-button-next',
+                prevEl : '.swiper-button-prev',
+        },
+    });
+
+
+
+    
+})
